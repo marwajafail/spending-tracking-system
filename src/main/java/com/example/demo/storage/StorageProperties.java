550c9c8a23342e54eb0a13586751c9abf239f618
marwa jafail <marwa.a.jafail@gmail.com>
2024-11-13 19:19:33 +0300

    add StorageProperties

---
+++ src/main/java/com/example/demo/storage/StorageProperties.java
@@ -0,0 +1,17 @@
+package com.example.demo.storage;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Setter
+@Getter
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+}
